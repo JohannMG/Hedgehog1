39c2e6a16da733c67b9cd81b9df2ada155353202
JohannMG <jgarces@national.aaa.com>
2016-01-07 09:42:06 -0500

    drafting day/night view to link to in table menu

---
+++ Hedgehog1/DayNightVC.h
@@ -0,0 +1,13 @@
+//
+//  DayNight.h
+//  Hedgehog1
+//
+//  Created by Garces, Johann (TEKsystems) on 1/7/16.
+//  Copyright © 2016 jmg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DayNight : UIViewController
+
+@end
